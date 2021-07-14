0e1b1480d1eaba8371df732bf88d9a7ee4c2546a
ruby <ruby@drpanda.com>
2021-07-14 16:09:16 +0800

    分页查询订单˚

--- src/main/java/com/yyww/mysql/jdbc/service/MainOrderService.java
+++ src/main/java/com/yyww/mysql/jdbc/service/MainOrderService.java
@@ -1,7 +1,10 @@
 package com.yyww.mysql.jdbc.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.yyww.mysql.jdbc.entity.MainOrderEntity;
+import com.yyww.mysql.jdbc.vo.OrderPage;
+import com.yyww.mysql.jdbc.vo.QueryOrderVo;
 
 /**
  * (MainOrder)表服务接口
@@ -10,4 +13,5 @@ import com.yyww.mysql.jdbc.entity.MainOrderEntity;
  * @since 2021-07-13 15:50:38
  */
 public interface MainOrderService extends IService<MainOrderEntity> {
+    IPage<OrderPage> queryPage(QueryOrderVo vo);
 }
--- src/main/java/com/yyww/mysql/jdbc/vo/OrderPage.java
+++ src/main/java/com/yyww/mysql/jdbc/vo/OrderPage.java
@@ -7,10 +7,28 @@ import java.util.Date;
 
 @Data
 public class OrderPage {
+    /**
+     * 订单Id
+     */
     private String orderId;
+    /**
+     * 创建时间
+     */
     private Date createTime;
+    /**
+     * 用户名
+     */
     private String username;
+    /**
+     *
+     */
     private String macId;
+    /**
+     * 性别
+     */
     private Integer gender;
+    /**
+     * 价格
+     */
     private BigDecimal price;
 }
